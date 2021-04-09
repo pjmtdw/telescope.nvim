301bfe41cca3017979740beaccfca2998c472ac1
TJ DeVries <devries.timothyj@gmail.com>
2021-04-09 14:09:32 -0400

    feat: Add ability to choose window to edit

--- lua/telescope/actions/set.lua
+++ lua/telescope/actions/set.lua
@@ -97,6 +97,9 @@ action_set.edit = function(prompt_bufnr, command)
     return
   end
 
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local win_id = picker.get_selection_window(picker, entry)
+
   local filename, row, col
 
   if entry.path or entry.filename then
@@ -130,8 +133,10 @@ action_set.edit = function(prompt_bufnr, command)
 
   local entry_bufnr = entry.bufnr
 
-  local picker = action_state.get_current_picker(prompt_bufnr)
   require("telescope.actions").close(prompt_bufnr)
+  if win_id ~= 0 and a.nvim_get_current_win() ~= win_id then
+    vim.api.nvim_set_current_win(win_id)
+  end
 
   if picker.push_cursor_on_edit then
     vim.cmd "normal! m'"
